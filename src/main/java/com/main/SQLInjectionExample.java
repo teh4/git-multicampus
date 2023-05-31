26ec072d72d8f94e7764380a172d2fb2634d67fe
teh4 <139567.naver.com>
2023-05-31 13:44:47 +0900

    commit 3

--- src/main/java/com/main/SQLInjectionExample.java
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -3,7 +3,7 @@ package com.main;
 public class SQLInjectionExample {
     public static void main(String[] args) {
         String username = getUserInput();
-        String query = "SELECT * FROM users WHERE username = '" + username + "'";
+        String query =  "SELECT * FROM users WHERE username = '" + username + "'";
         // query 실행 로직
     }
 
